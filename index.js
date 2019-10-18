93fcb5b059e39bc80ab29e7b3338253955fc4b26
Guizeto <g.luizeto@hotmail.com>
2019-10-17 23:07:57 -0300

    first code

--- index.js
+++ index.js
@@ -1,46 +1,77 @@
 const express = require('express');
 
 const server = express();
-server.use(express.json())
-
-
-function checkProjectExist (req, res, next) {
-  const { project } = req.body;
-
-  next();
-}
+server.use(express.json());
 
 const projects = [];
+let countReq = 0;
 
-// { id: "1", title: 'Novo projeto', tasks: [] }
+function handleProjectExist(req, res, next){
+    const {id} = req.params
+    const isExcist = projects.find(item => item.id === id)
+
+    if(!isExcist)
+        return res.json({msg: 'User not exist'})
+
+        next();
+}
+
+function handleCountReq (req, res, next) {
+    countReq++
+    console.log(countReq);
+    next();
+}
+
+server.use(handleCountReq);
+
+//Rotas de boas-vindas
+server.get('/', (req, res) => {
+    return res.json({ msg: 'Wellcome! :)' })
+})
+
 server.post('/projects', (req, res) => {
-  const project = req.body;
-  projects.push(project);
-  return res.json({msg :'Registered Product', project})
+    const { title, id } = req.body;
+    const project = {
+        title,
+        id,
+        tasks: []
+    }
+    projects.push(project)
+
+    return res.json({ msg: 'Create Success!', projects })
 })
 
-server.get('/projects',  (req, res) => {
-  const {project} = req.body;
-
-  return res.json(projects)
+server.get('/projects', (req, res) => {
+    return res.json({ msg: 'GET /projects', projects })
 })
 
-server.put('/projects',  (req, res) => {
-  const {project} = req.body;
+server.put('/projects/:id', handleProjectExist, (req, res) => {
+    const { id } = req.params
+    const { title } = req.body
+    const project = projects.find(item => item.id === id)
+    project.title = title
 
-  return res.json({msg :'Registered Product'})
+    return res.json({ msg: 'Update Success', project })
 })
 
-server.delete('/projects',  (req, res) => {
-  const {project} = req.body;
+server.delete('/projects/:id', handleProjectExist, (req, res) => {
+    const {id} = req.params
 
-  return res.json({msg :'Registered Product'})
+    const index = projects.findIndex(item => item.id === id)
+
+    projects.splice(index, 1)
+
+    return res.send();
 })
 
-server.post('/projects',  (req, res) => {
-  const {project} = req.body;
+server.post('/projects/:id/tasks', handleProjectExist, (req, res) => {
+    const {id} = req.params;
+    const {title} = req.body
 
-  return res.json({msg :'Registered Product'})
+    const project = projects.find(item => item.id === id);
+    project.tasks.push(title);
+
+    return res.json({ msg: 'Create Success!', project })
 })
 
-server.listen(3000)
+server.listen(3333);
